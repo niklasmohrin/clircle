2a39b8ee92d2833c84c1498cf8e59205a2ae89a3
Niklas Mohrin <niklas.mohrin@gmail.com>
2020-11-01 19:40:02 +0100

    Add return code example

---
+++ examples/return_code.rs
@@ -0,0 +1,23 @@
+//! This example collects all given arguments, interprets them as paths, gathers information
+//! about them and checks if stdout is being redirected to one of the given files.
+//! If that's the case, the return code will be set to 1. In both cases, there is an according
+//! output to stderr.
+use clircle::{stdout_among_inputs, Identifier};
+use std::convert::TryFrom;
+
+fn main() {
+    let inputs: Vec<_> = std::env::args().collect();
+    let inputs: Result<Vec<Identifier>, _> = inputs
+        .iter()
+        .map(AsRef::as_ref)
+        .map(Identifier::try_from)
+        .collect();
+    let inputs = inputs.expect("There was an argument that could not be converted to an Idenfier!");
+
+    if stdout_among_inputs(&inputs) {
+        eprintln!("Cycle detected!");
+        std::process::exit(1);
+    } else {
+        eprintln!("No cycle detected.")
+    }
+}
